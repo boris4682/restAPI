eb5fb537c5e8ab9ede556e76d1ba4ca6533cff25
Boris Prach <b.prach@worksolutions.ru>
2020-09-08 22:28:32 +0300

    Тестирование подключения к БД

--- app/rest.php
+++ app/rest.php
@@ -1,3 +1,16 @@
 <?php
 
-echo phpinfo();
+
+
+try {
+    $con = new PDO(
+        "mysql:host=db;dbname=orders;charset=utf8",
+        'root',
+        'password');
+    $sth = $con->prepare("SELECT * FROM `orders` WHERE `id` = :id");
+    $sth->execute(array('id' => '6'));
+    $array = $sth->fetch(PDO::FETCH_ASSOC);
+    print_r($array);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
